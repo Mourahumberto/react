52f066244ca5be47cd2b4abd46bdbdd5a96d395d
Humberto Moura <humbertomoura.n@gmail.com>
2025-01-06 17:10:33 -0300

    liesta de tarefas

---
+++ listatarefas/src/components/Main.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+
+// Form
+import {FaPlus} from 'react-icons/fa'
+
+import './Main.css'
+export default class Main extends Component {
+    state={
+        novaTarefa: '',
+        tarefa: [
+            'Fazer café',
+            'Estudar',
+        ]
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            novaTarefa: e.target.value,
+        });
+    }
+    render() {
+        const { novaTarefa, tarefas } = this.state
+        return (
+            <div className="main">
+                <h1>Lista de tarefas</h1>
+
+                <form action="#" className="form">
+                    <input
+                     onChange={this.handleChange}
+                     type="text"
+                     value={novaTarefa} />
+                    <button type="submit">
+                        <FaPlus />
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
